42bff192a1ca3a113a2c84e2b7a76e88c193542a
1000ship <cjstjdgur1234@gmail.com>
2020-07-10 17:28:43 +0900

    자잘한 수정사항들 처리

--- 01/js/main.js
+++ 01/js/main.js
@@ -13,20 +13,108 @@
         messageD: document.querySelector("#scroll-section-0 .main-message-d"),
       },
       values: [
-        {extra: {left:true, right:false}, low: 0, high: 1, range: { start: 0.1, end: 0.2 }, obj: 'messageA'},
-        {extra: {left:false,right: true}, low: 1, high: 0, range: { start: 0.1+0.1, end: 0.2+0.1 }, obj: 'messageA'},
-        {extra: {left:true, right:false}, low: 0, high: 1, range: { start: 0.3, end: 0.4 }, obj: 'messageB'},
-        {extra: {left:false,right: true}, low: 1, high: 0, range: { start: 0.3+0.1, end: 0.4+0.1 }, obj: 'messageB'},
-        {extra: {left:true, right:false}, low: 0, high: 1, range: { start: 0.5, end: 0.6 }, obj: 'messageC'},
-        {extra: {left:false,right: true}, low: 1, high: 0, range: { start: 0.5+0.1, end: 0.6+0.1 }, obj: 'messageC'},
-        {extra: {left:true, right:false}, low: 0, high: 1, range: { start: 0.7, end: 0.8 }, obj: 'messageD'},
-        {extra: {left:false,right: true}, low: 1, high: 0, range: { start: 0.7+0.1, end: 0.8+0.1 }, obj: 'messageD'},
+        {
+          extra: { left: true, right: false },
+          low: 0,
+          high: 1,
+          range: { start: 0.1, end: 0.2 },
+          obj: "messageA",
+          prop: "opacity"
+        },
+        {
+          extra: { left: false, right: true },
+          low: 1,
+          high: 0,
+          range: { start: 0.1 + 0.1, end: 0.2 + 0.1 },
+          obj: "messageA",
+          prop: "opacity"
+        },
+        {
+          low: 10,
+          high: -10,
+          range: {start: 0.1, end: 0.1 + 0.2},
+          obj: "messageA",
+          prop: "margin-top",
+          format: (value) => `${value}px`
+        },
+        {
+          extra: { left: true, right: false },
+          low: 0,
+          high: 1,
+          range: { start: 0.3, end: 0.4 },
+          obj: "messageB",
+          prop: "opacity"
+        },
+        {
+          extra: { left: false, right: true },
+          low: 1,
+          high: 0,
+          range: { start: 0.3 + 0.1, end: 0.4 + 0.1 },
+          obj: "messageB",
+          prop: "opacity"
+        },
+        {
+          low: 10,
+          high: -10,
+          range: {start: 0.3, end: 0.3 + 0.2},
+          obj: "messageB",
+          prop: "margin-top",
+          format: (value) => `${value}px`
+        },
+        {
+          extra: { left: true, right: false },
+          low: 0,
+          high: 1,
+          range: { start: 0.5, end: 0.6 },
+          obj: "messageC",
+          prop: "opacity"
+        },
+        {
+          extra: { left: false, right: true },
+          low: 1,
+          high: 0,
+          range: { start: 0.5 + 0.1, end: 0.6 + 0.1 },
+          obj: "messageC",
+          prop: "opacity"
+        },
+        {
+          low: 10,
+          high: -10,
+          range: {start: 0.5, end: 0.5 + 0.2},
+          obj: "messageC",
+          prop: "margin-top",
+          format: (value) => `${value}px`
+        },
+        {
+          extra: { left: true, right: false },
+          low: 0,
+          high: 1,
+          range: { start: 0.7, end: 0.8 },
+          obj: "messageD",
+          prop: "opacity"
+        },
+        {
+          extra: { left: false, right: true },
+          low: 1,
+          high: 0,
+          range: { start: 0.7 + 0.1, end: 0.8 + 0.1 },
+          obj: "messageD",
+          prop: "opacity"
+        },
+        {
+          low: 10,
+          high: -10,
+          range: {start: 0.7, end: 0.7 + 0.2},
+          obj: "messageD",
+          prop: "margin-top",
+          format: (value) => `${value}px`
+        },
       ],
     },
     {
       // 1
       type: "normal",
-      heightNum: 5,
+      heightNum: 1.5,
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-1"),
@@ -55,10 +143,12 @@
   function setSetting() {
     // 각 스크롤 섹션의 높이 세팅
     sceneInfo.forEach((info) => {
-      info.scrollHeight = info.heightNum * window.innerHeight;
+      if (info.type === "sticky")
+        info.scrollHeight = info.heightNum * window.innerHeight;
+      else if (info.type === "normal")
+        info.scrollHeight = info.objs.container.offsetHeight;
       info.objs.container.style.height = `${info.scrollHeight}px`;
     });
-    console.log(sceneInfo);
   }
 
   let yOffset = 0;
@@ -89,7 +179,7 @@
     let rv;
     const scrollHeight = sceneInfo[currentScene].scrollHeight;
     const scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-    const {low, high, range, extra} = values;
+    const { low, high, range, extra } = values;
     if (range) {
       const { start, end } = range;
       const partScrollStart = start * scrollHeight;
@@ -116,16 +206,17 @@
 
     switch (currentScene) {
       case 0:
-        values.forEach( value => {
-          const {obj, extra, range} = value;
-          if(extra && range){
-            if(!extra.left && scrollRatio < range.start)
-              return;
-            if(!extra.right && scrollRatio > range.end)
-              return;
+        values.forEach((value) => {
+          const { obj, extra, range, prop, format } = value;
+          if (extra && range) {
+            if (!extra.left && scrollRatio < range.start) return;
+            if (!extra.right && scrollRatio > range.end) return;
           }
-          objs[obj].style.opacity = calcValues( value, currentYOffset )
-        })
+          if( format )
+            objs[obj].style[prop] = format(calcValues(value, currentYOffset));
+          else
+            objs[obj].style[prop] = calcValues(value, currentYOffset);
+        });
         break;
       case 1:
         console.log("1 play");
