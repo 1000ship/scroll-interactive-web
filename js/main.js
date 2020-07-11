2dcad5292106e7c12785c31581f0bf08f55bf0ca
1000ship <cjstjdgur1234@gmail.com>
2020-07-11 14:51:39 +0900

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function () {
+  let loadedCount = 0;
   const totalFrame = 80;
   const frameImages = [];
   const imagePath = (frame) => `./img/puppy/frame-${frame}.jpg`;
@@ -6,27 +7,37 @@
 
   const $bg = document.getElementById("bgImage");
   const $img = document.getElementById("scrollImage");
+  const $loadingText = document.getElementById("loadingText");
 
   function loadImages() {
     for (let i = 0; i < totalFrame; ++i) {
       let image = new Image();
       image.src = imagePath(i);
       frameImages.push(image);
+      image.addEventListener("load", () => {
+        loadedCount += 1;
+        $loadingText.innerHTML = `Now Loading... ${
+          parseInt(loadedCount / totalFrame) * 100
+        }%`;
+        if (loadedCount === totalFrame) loadedAllFrame();
+      });
     }
   }
 
-  window.addEventListener("scroll", () => {
-    const scrollRate =
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-    let currentFrame = parseInt(totalFrame * scrollRate);
-    if (currentFrame >= totalFrame) currentFrame = totalFrame - 1;
-    if (currentFrame < 0) currentFrame = 0;
-    $img.src = frameImages[ currentFrame ].src
-    $bg.style.backgroundImage = `url(${imagePath(currentFrame)})`;
-  });
+  function loadedAllFrame() {
+    $loadingText.style.display = "none";
+    window.addEventListener("scroll", () => {
+      const scrollRate =
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+      let currentFrame = parseInt(totalFrame * scrollRate);
+      if (currentFrame >= totalFrame) currentFrame = totalFrame - 1;
+      if (currentFrame < 0) currentFrame = 0;
+      $img.src = frameImages[currentFrame].src;
+      $bg.style.backgroundImage = `url(${imagePath(currentFrame)})`;
+    });
+  }
 
-  window.addEventListener('DOMContentLoaded', ()=>{
-    console.log( 'loaded')
+  window.addEventListener("DOMContentLoaded", () => {
     loadImages();
-  })
+  });
 })();
